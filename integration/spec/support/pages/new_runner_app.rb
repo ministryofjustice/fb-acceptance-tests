7b4b881748ba7dcc02d4de11ed7910eee0cc09fd
brenetic <brendan.butler@digital.justice.gov.uk>
2021-06-29 16:52:16 +0100

    Add v2 runner optional file upload tests
    This checks whether it's possible for a user to add a file and then
    remove it and that the file name does not exist on the check your
    answers page or the generated PDF.

--- integration/spec/support/pages/new_runner_app.rb
+++ integration/spec/support/pages/new_runner_app.rb
@@ -4,4 +4,6 @@ class NewRunnerApp < FeaturesEmailApp
   element :start_button, :button, 'Start now'
   element :yes_field, :radio_button, 'Yes', visible: false
   element :change_optional_checkbox, :link, text: 'Your answer for Optional checkboxes (Optional)', visible: false
+  element :change_optional_file_upload, :link, text: 'Your answer for Optional file upload (Optional)', visible: false
+  element :remove_file, :link, text: 'Remove file'
 end
