52701c89c642b8d087ce5ad7fcf0788c8e375302
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-04-05 14:14:08 +0100

    Complete submit spec

--- integration/spec/features/v2/save_and_return_spec.rb
+++ integration/spec/features/v2/save_and_return_spec.rb
@@ -22,10 +22,27 @@ describe 'Save and return' do
     form.question_1.set('hello')
     continue
     form.save_and_return.click
-    form.email.set('email@cool.com')
+    form.email.set('email@')
+    continue
+    check_validation_error_message('Enter an email address in the correct format, like name@example.com')
+    form.email.set('email@example.com')
+    continue
+    check_validation_error_message("can't be blank")
     form.secret_question_1.choose
     form.secret_question_2.choose
     form.secret_question_3.choose
+    continue
+    check_validation_error_message("can't be blank")
     form.secret_answer.set('foo')
+    form.secret_question_1.choose
+    continue
+    check_optional_text(page.text)
+    form.email_confirmation.set('email@email.com')
+    check_validation_error_message("Check your email address is correct")
+    form.email_confirmation.set('email@example.com')
+    continue
+    check_optional_text(page.text)
+    expect(page.text).to include('Your form has been saved')
+    expect(page.text).to include('We have sent a one-off link to email@example.com')
   end
 end
--- integration/spec/support/pages/save_and_return_v2_app.rb
+++ integration/spec/support/pages/save_and_return_v2_app.rb
@@ -12,17 +12,7 @@ class SaveAndReturnV2App < FeaturesEmailApp
   element :secret_question_1, :radio_button, 'What is your mother\'s maiden name?', visible: false
   element :secret_question_2, :radio_button, 'What is the last name of your favourite teacher?', visible: false
   element :secret_question_3, :radio_button, 'What is the name of the hospital where you were born?', visible: false
-
-  # element :change_optional_checkbox, :link, text: 'Your answer for Optional checkboxes (Optional)', visible: false
-  # element :change_optional_file_upload, :link, text: 'Your answer for Optional file upload (Optional)', visible: false
-  # element :remove_file, :link, text: 'Remove file'
-  # element :change_first_name, :link, text: 'Your answer for First name', visible: false
-  # element :change_last_name, :link, text: 'Your answer for Last name', visible: false
-  # element :change_email, :link, text: 'Your answer for Your email address', visible: false
-  # element :change_cat, :link, text: 'Your answer for Your cat', visible: false
-  # element :change_upload, :link, text: 'Your answer for Upload a file', visible: false
-  # element :change_autocomplete, :link, text: 'Where do you like to holiday?', visible: false
-  # element :autocomplete_countries_field, :field, 'Where do you like to holiday?'
+  element :email_confirmation, :field, 'Email Confirmation'
 
   def load(expansion_or_html = {}, &block)
     puts "Visiting form: #{ENV['SAVE_AND_RETURN_V2_APP'] % { user: '*****', password: '*****' }}"
