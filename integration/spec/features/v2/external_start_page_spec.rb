48e6dbeb26941755f7f6bef39d3fdceb7d07dde5
Natalie Seeto <njseeto@users.noreply.github.com>
2023-08-24 14:25:25 +0100

    Update dependabot
    - remove nat
    
    External start page spec

---
+++ integration/spec/features/v2/external_start_page_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'Page with external start page' do
+  # before :each do
+  #   OutputRecorder.cleanup_recorded_requests if ENV['CI_MODE'].blank?
+  # end
+  let(:form) { EternalStartPageApp.new }
+
+  # use same username and password as new runner acceptance test
+  # let(:username) { ENV['NEW_RUNNER_ACCEPTANCE_TEST_USER'] }
+  # let(:password) { ENV['NEW_RUNNER_ACCEPTANCE_TEST_PASSWORD'] }
+  let(:username) { 'picard' }
+  let(:password) { 'makeitso' }
+  let(:q1_answer) { 'Hello' }
+
+  # before { form.load }
+  # comment above line and uncomment below and export user and password ENV vars for local testing
+  before { visit "https://#{username}:#{password}@external-start-page-acceptance-test.dev.test.form.service.justice.gov.uk/" }
+
+  it 'uses an external start page' do
+    # bypasses start page
+    expect(page.text).to include('First Question')
+    # header link is set to the external start page
+    expect(page).to have_link('external-start-page-acceptance-test', href: 'https://gov.uk')
+    form.question_1.set(q1_answer)
+    continue
+    expect(page.text).to include('Check you answers')
+    visit "https://#{username}:#{password}@external-start-page-acceptance-test.dev.test.form.service.justice.gov.uk/"
+    sleep 1
+    # bypasses start page on revisit
+    expect(page.text).to include('First Question')
+    # session and previous answer still present
+    expect(page.text).to include(q1_answer)
+  end
+end
