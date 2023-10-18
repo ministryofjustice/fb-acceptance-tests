d13eff10a3754b0ebc9124eca249445286af9164
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-10-18 16:48:29 +0100

    Adding first test to check is content always present is shown

--- integration/spec/features/v2/conditional_content_spec.rb
+++ integration/spec/features/v2/conditional_content_spec.rb
@@ -1,11 +1,18 @@
 describe 'Conditional Content' do
   let(:form) { ConditionalContentV2App.new }
+  let(:always_content) { 'This is always shown' }
 
   before { form.load }
   # comment above line and uncomment below and export user and password ENV vars for local testing
   # before { visit "https://#{username}:#{password}@new-runner-acceptance-tests.dev.test.form.service.justice.gov.uk" }
 
-  it 'shows appropriate conditional content depending on user answers' do
 
+  it 'shows appropriate conditional content depending on users answers' do
+    form.start_now_button.click
+    form.radio_a.choose
+    continue
+    form.checkbox_1.check
+    continue
+    expect(page.text).to include(always_content)
   end
 end
--- integration/spec/support/pages/conditional_content_v2_app.rb
+++ integration/spec/support/pages/conditional_content_v2_app.rb
@@ -3,4 +3,10 @@ class ConditionalContentV2App < ServiceApp
     user: ENV['NEW_RUNNER_ACCEPTANCE_TEST_USER'],
     password: ENV['NEW_RUNNER_ACCEPTANCE_TEST_PASSWORD']
   }
+
+  element :checkbox_1, :checkbox, 'Option 1', visible: false
+  element :checkbox_2, :checkbox, 'Option 2', visible: false
+  element :checkbox_O, :checkbox, 'Option', visible: false
+  element :radio_a, :radio_button, 'a', visible: false
+  element :radio_b, :radio_button, 'b', visible: false
 end
