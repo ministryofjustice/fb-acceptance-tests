d15c6751af37020f7355be82debd219408ef48ff
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-10-19 08:32:41 +0100

    Checking legacy content is displayed

--- integration/spec/features/v2/conditional_content_spec.rb
+++ integration/spec/features/v2/conditional_content_spec.rb
@@ -2,6 +2,7 @@ describe 'Conditional Content' do
   let(:form) { ConditionalContentV2App.new }
   let(:always_content) { 'This is always shown' }
   let(:never_content) { 'This is never shown' }
+  let(:legacy_content) { 'This is legacy conditional content that should be picked up by default' }
 
   before { form.load }
   # comment above line and uncomment below and export user and password ENV vars for local testing
@@ -18,5 +19,7 @@ describe 'Conditional Content' do
     expect(page.text).not_to include(never_content)
     expect(page.text).to include('If radio a && checkbox Option 1')
     expect(page.text).to include('If radio is a && checkbox is 1 OR radio is b && checkbox is 2')
+    continue
+    expect(page.text).to include(legacy_content)
   end
 end
