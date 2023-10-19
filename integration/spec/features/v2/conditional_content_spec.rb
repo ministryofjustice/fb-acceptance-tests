3af0b8311de8364eec1907a8872068195f9e40cb
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-10-19 17:23:12 +0100

    Check checkbox contains only catches the answer

--- integration/spec/features/v2/conditional_content_spec.rb
+++ integration/spec/features/v2/conditional_content_spec.rb
@@ -4,7 +4,7 @@ describe 'Conditional Content' do
   let(:never_content) { 'This is never shown' }
   let(:logic_combination_content) { 'If radio is a && checkbox is 1 OR radio is b && checkbox is 2' }
   let(:negative_logic_combination) { 'If radio is not a OR checkbox is not Option 1 OR checkbox is not Option 2' }
-
+  let(:checkbox_contains_substring) { 'If checkbox contains Option, it should not show if Option 1 or Option 2' }
 
   before { form.load }
   # comment above line and uncomment below and export user and password ENV vars for local testing
@@ -29,6 +29,17 @@ describe 'Conditional Content' do
     expect(page.text).to include(always_content)
     expect(page.text).not_to include(never_content)
     expect(page.text).to include('If radio b II checkbox Option 2')
+    expect(page.text).to include(negative_logic_combination)
+    expect(page.text).not_to include(checkbox_contains_substring)
+
+    form.back.click
+    form.checkbox_2.uncheck
+    form.checkbox_0.check
+    continue
+    expect(page.text).to include(always_content)
+    expect(page.text).not_to include(never_content)
+    expect(page.text).to include('If radio a && checkbox Option 1')
+    expect(page.text).to include(checkbox_contains_substring)
     expect(page.text).to include(negative_logic_combination)
   end
 end
