e5113c8ce96f947076267e8df5c29f2c60b079e2
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-10-25 17:17:43 +0100

    Conditional content (#369)
    * Adding squeleton for conditional content tests
    
    * Adding first test to check is content always present is shown
    
    * Adding all conditional content expected with this selection
    
    * Testing never conditional content is not displayed
    
    * Checking legacy content is displayed
    
    * Removing legacy test for now
    
    * Adding back link element
    
    * Adding tests with negative logic combination content
    
    * Check checkbox contains only catches the answer
    
    * Adding some comments
    
    * Adding test case for unanswered optional checkbox
    
    * Adding further tests to check the right side of the logic expression
    
    * Testing legacy content is being shown
    
    * Fix failing test

---
+++ integration/spec/support/pages/conditional_content_v2_app.rb
@@ -0,0 +1,13 @@
+class ConditionalContentV2App < ServiceApp
+  set_url "#{ENV['CONDITIONAL_CONTENT_V2_APP']}" % {
+    user: ENV['NEW_RUNNER_ACCEPTANCE_TEST_USER'],
+    password: ENV['NEW_RUNNER_ACCEPTANCE_TEST_PASSWORD']
+  }
+
+  element :checkbox_1, :checkbox, 'Option 1', visible: false
+  element :checkbox_2, :checkbox, 'Option 2', visible: false
+  element :checkbox_0, :checkbox, 'Option', visible: false
+  element :radio_a, :radio_button, 'a', visible: false
+  element :radio_b, :radio_button, 'b', visible: false
+  element :back, :link, text: 'Back', visible: false
+end
