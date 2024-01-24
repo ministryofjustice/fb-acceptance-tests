b0652885f568c8d44ef481007bdc25e874ee5f8d
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-01-24 14:41:57 +0000

    Address component (#388)
    * Setting deployment form for testing
    
    * Adding tests for address component
    
    Co-authored-by: Jesus Laiz <jesus.laiz@digital.justice.gov.uk>
    
    * Using the same form name to be found in the the mailbox
    
    Co-authored-by: Jesus Laiz <jesus.laiz@digital.justice.gov.uk>
    
    * Referencing last version of the acceptance tests form
    
    * Tests was not getting to the CYA page
    
    * Referencing the real runner form
    
    * Moving elements with all the other elements that are not links
    
    Co-authored-by: Jesus Laiz <jesus.laiz@digital.justice.gov.uk>
    
    * Setting fields in not visible
    
    * Removing visible caracteristic and adding some checks
    
    Co-authored-by: Jesus Laiz <jesus.laiz@digital.justice.gov.uk>
    
    * Just running v2 tests
    
    * Fixing csv test
    
    * Adding a step to take into account of the new page in the form while made the address question optional
    
    * Set back all acceptances to run
    
    ---------
    
    Co-authored-by: Jesus Laiz <jesus.laiz@digital.justice.gov.uk>

--- integration/spec/support/pages/features_email_app.rb
+++ integration/spec/support/pages/features_email_app.rb
@@ -17,4 +17,8 @@ class FeaturesEmailApp < ServiceApp
   element :autocomplete_field, '#page_autocomplete--autocomplete_autocomplete', visible: false
   element :cat_picture_field, :file_field, 'What does your cat look like?'
   element :confirm_upload_field, :radio_button, 'Yes, add this upload', visible: false
+  element :autocomplete_countries_field, :field, 'Where do you like to holiday?'
+  element :address_line_1, :field, 'Address line 1'
+  element :city, :field, 'Town or city'
+  element :postcode, :field, 'Postcode'
 end
