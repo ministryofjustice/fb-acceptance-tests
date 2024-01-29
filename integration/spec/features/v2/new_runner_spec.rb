dfaf1ac3bd766bce586b38d3f60867ecb094c66a
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-01-29 16:27:48 +0000

    ?

--- integration/spec/features/v2/new_runner_spec.rb
+++ integration/spec/features/v2/new_runner_spec.rb
@@ -320,8 +320,7 @@ describe 'New Runner' do
     expect(result).to include('hello_world_multi_2.txt')
     
     # optional multi file upload
-    expect(result).to match(/Optional multi file/)
-    expect(result).to match(/upload (optional)/)
+    expect(result).to include('Optional multi file')
 
     # autocomplete
     expect(result).to include('Where do you like to')
