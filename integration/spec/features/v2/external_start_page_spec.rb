640b0b8db62d4a3a23fc66c533e16fca19dba814
Jesus Laiz <zheileman@users.noreply.github.com>
2024-04-03 17:35:56 +0100

    Final touches

--- integration/spec/features/v2/external_start_page_spec.rb
+++ integration/spec/features/v2/external_start_page_spec.rb
@@ -19,7 +19,7 @@ describe 'Page with external start page' do
     form.question_1.set(q1_answer)
     continue
     expect(page.text).to include('Check your answers')
-    visit ENV['EXTERNAL_START_PAGE_APP']
+    form.service_header_link.click
     sleep 1
     # bypasses start page on revisit
     expect(page.text).to include(q1_title)
